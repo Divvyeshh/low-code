df0bc515e2696b26dbfe666bf25191272275c2f2
Divvyeshh <divyesh.shivakumar2021@vitstudent.ac.in>
2023-08-24 16:54:59 +0530

    title size

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -3,6 +3,7 @@ import './Dash.css';
 import InputOptions from './inputOptions';
 import GeneratedCode from './GenerateHTML';
 import InputBoxRenderer from './renderInput';
+import TitleSize from './titleSize';
 import Title from './title';
 import Grid from '@mui/material/Grid';
 
@@ -15,8 +16,22 @@ class Dash extends Component {
       name: '',
       formTitle: '',
       subTitle: '',
+      size: '1',
+      subsize: '1',
     };
   }
+  
+  handleSize = (val) => {
+    this.setState({
+      size: val,
+    });
+  };
+
+  handleSubSize = (num) => {
+    this.setState({
+      subsize: num,
+    });
+  };
 
   handleType = (event) => {
     this.setState({
@@ -50,8 +65,8 @@ class Dash extends Component {
 
   generateInputCode = () => {
     return (
-`<h1>${this.state.formTitle}</h1>
-<h2>${this.state.subTitle}</h2>
+`<h${this.state.size}>${this.state.formTitle}</h${this.state.size}>
+<h${this.state.subsize}>${this.state.subTitle}</h${this.state.subsize}>
 <form>
       <input type="${this.state.type}" name="${this.state.name}" placeholder="${this.state.placeholder}" />
 </form>`
@@ -64,6 +79,7 @@ class Dash extends Component {
       <Grid container spacing={2}>
 
         <Grid item xs={4} className='column'>
+          <TitleSize handleSize={this.handleSize} handleSubSize={this.handleSubSize}/>
           <Title formTitle={this.state.formTitle} handleFormTitle={this.handleFormTitle} subTitle={this.state.subTitle} handleSubtitle={this.handleSubtitle}/>
           <InputOptions
             type={this.state.type}
